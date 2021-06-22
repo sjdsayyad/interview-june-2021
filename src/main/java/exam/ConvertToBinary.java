d9190acabb056e80fe2bb6683ef48cb5b12cd302
Sajid Munir Sayyad <55912275+sjdsayyad@users.noreply.github.com>
2021-06-22 12:50:05 +0530

    Update ConvertToBinary.java

--- src/main/java/exam/ConvertToBinary.java
+++ src/main/java/exam/ConvertToBinary.java
@@ -1,9 +1,30 @@
-package exam;
+import java.util.*;  
+import java.io.*;
+public class convert {
+    static void printBinary(int[] binary, int id)
+    {
+       for (int i = id - 1; i >= 0; i--)
+            System.out.print(binary[i] + "");
+    }
 
-public class ConvertToBinary {
+    public static void decimalToBinary(int n)
+    {
+        int[] binary = new int[35];
+        int id = 0;
 
-  public String toBinary(final int n) {
-    //TODO write your code here to achieve the desired result as explained in Readme file for this problem.
-    return "";
-  }
+        while (n > 0) {
+            binary[id++] = n % 2;
+            n = n / 2;
+        }
+
+        printBinary(binary, id);
+    }
+
+    public static void main(String[] args)
+    {
+        Scanner sc= new Scanner(System.in);    
+        System.out.print("Enter number- ");  
+        int number= sc.nextInt();
+        decimalToBinary(number);
+    }
 }
